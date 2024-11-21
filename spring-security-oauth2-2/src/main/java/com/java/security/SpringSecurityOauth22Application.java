70a5ac88136139b002bb4a235dc88a26db3c3c06
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2024-11-21 12:19:48 +0530

    [refactor]

---
+++ spring-security-oauth2-2/src/main/java/com/java/security/SpringSecurityOauth22Application.java
@@ -0,0 +1,13 @@
+package com.java.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOauth22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOauth22Application.class, args);
+	}
+
+}
