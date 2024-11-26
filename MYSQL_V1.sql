71840cc3f8f3ee60100febecf92151d85c195438
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2024-11-26 15:09:57 +0530

    [refactor]

--- MYSQL_V1.sql
+++ MYSQL_V1.sql
@@ -14,8 +14,3 @@ SELECT `roles`.`id`,
     `roles`.`student_id`,
     `roles`.`name`
 FROM `student_database`.`roles`;
-
-INSERT INTO `student_database`.`roles`(`id`,`student_id`,`name`)VALUES('8faeb3de-44b4-4560-ac62-0b2fb1246855',9999999999,"ADMIN");
-
-INSERT INTO `student_database`.`roles`(`id`,`student_id`,`name`)VALUES('b6611e15-3a94-41f7-8b20-a295f11edf50',1111111112,"BASIC");
-
