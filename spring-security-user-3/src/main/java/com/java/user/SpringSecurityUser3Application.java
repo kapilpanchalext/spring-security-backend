6c321d4245a637a122218b0f39c356a3f6fe1cd2
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2024-11-26 12:03:20 +0530

    [refactor]

---
+++ spring-security-user-3/src/main/java/com/java/user/SpringSecurityUser3Application.java
@@ -0,0 +1,13 @@
+package com.java.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityUser3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityUser3Application.class, args);
+	}
+
+}
